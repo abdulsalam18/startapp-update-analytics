2f44963729b14497494f679199656c5f6edc1af4
Game Build <abdulsalam01724258670@gmail.com>
2026-03-26 01:06:08 +0600

    Update update-version.js

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -19,23 +19,4 @@ module.exports = function (ctx) {
 
     console.log("✅ Root config.xml updated");
 
-    // Android platform config
-    const androidConfig = path.join(
-        ctx.opts.projectRoot,
-        'platforms/android/app/src/main/res/xml/config.xml'
-    );
-
-    if (fs.existsSync(androidConfig)) {
-        let xml2 = fs.readFileSync(androidConfig, 'utf8');
-
-        xml2 = xml2.replace(
-            /<widget([^>]*)version="([^"]*)"/,
-            '<widget$1version="1.0.0" android-versionCode="100"'
-        );
-
-        fs.writeFileSync(androidConfig, xml2);
-
-        console.log("✅ Android config.xml updated");
-    }
-
 };
